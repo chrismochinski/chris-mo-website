f5aa04cc14fa1f2f24466a97bd55d9ff5a8b8a1e
Chris "Mo" Mochinski <cmochinski@gmail.com>
2021-11-03 12:02:07 -0500

    begin code cleanup

--- src/components/AboutMeSection/AboutMeElements.js
+++ src/components/AboutMeSection/AboutMeElements.js
@@ -1,22 +1,21 @@
 import styled from "styled-components";
 
 export const AboutMeContainer = styled.div`
-   color: #fff;
-  background: ${({ lightBg }) => (lightBg ? '#f9f9f9' : '#010606')};
+  color: #fff;
+  background: ${({ lightBg }) => (lightBg ? "#f9f9f9" : "#010606")};
 
   @media screen and (max-width: 768px) {
     padding: 100px 0;
   }
 
   @media screen and (max-width: 768px) {
-padding: 0 0;
+    padding: 0 0;
   }
 `;
 
-export const AboutMeWrapper = styled.div`  
-
-align-items: center;
-   display: grid;
+export const AboutMeWrapper = styled.div`
+  align-items: center;
+  display: grid;
   z-index: 1;
   height: 860px;
   width: 100%;
@@ -25,18 +24,16 @@ align-items: center;
   margin-left: auto;
   padding: 0 24px;
   justify-content: center;
-
 `;
 
 export const AboutMeRow = styled.div`
   display: grid;
   grid-auto-columns: minmax(auto, 1fr); //research
   align-items: center;
-
 `;
 
 export const Column1 = styled.div`
-margin-bottom: 15px;
+  margin-bottom: 15px;
   padding: 0 15px;
 `;
 
@@ -45,29 +42,25 @@ export const TextWrapper = styled.div`
   padding-top: 0;
   padding-bottom: 60px;
   text-align: center;
-
 `;
 
 export const TopLine = styled.div`
-  color: #309DE7;
+  color: #309de7;
   font-size: 20px;
   line-height: 16px; //research
   font-weight: 700;
   letter-spacing: 1.4px; //research
   text-transform: uppercase; //research
   margin-bottom: 12px;
-
 `;
 
 export const Heading = styled.h1`
-
   margin-bottom: 24px;
   font-size: 48px;
   line-height: 1.1;
   font-weight: 600;
   color: ${({ lightText }) => (lightText ? "#f7f8fa" : "#010606")};
 
-  
   @media screen and (max-width: 480px) {
     font-size: 32px;
   }
@@ -86,7 +79,6 @@ export const Subtitle = styled.p`
   }
 `;
 
-
 export const ImgWrap = styled.div`
   max-width: 400px;
   height: 100%;
@@ -100,5 +92,4 @@ export const Img = styled.img`
   margin-right: 0;
   margin-left: 0;
   padding-right: 0;
-  
 `;
